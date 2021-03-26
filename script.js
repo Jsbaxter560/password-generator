0e0706b9c306c82e697dbb97209d60e2bcd8b8e1
John Baxter <juancho420@ymail.com>
2021-03-26 16:35:40 -0700

    initial pseudocode

--- script.js
+++ script.js
@@ -12,3 +12,15 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+
+// use prompts to extract input from user 
+// confirm user criteria for password:
+// length of password 8-128 char
+// lowercase? T or F
+// uppercase? T or F
+// numerals? T or F
+// special char? T or F
+// when criteria confirmed display random examples from corresponding array
+// use math floo and math random to generate password from corresponding arrays
+// display generated password in alert window or append to page
