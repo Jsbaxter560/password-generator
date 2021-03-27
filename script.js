d59fef68be3502108f995e28bcb4399b4ac2ea22
John Baxter <juancho420@ymail.com>
2021-03-26 20:33:41 -0700

    testing of first prompt window

--- script.js
+++ script.js
@@ -1,17 +1,35 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
-// Write password to the #password input
-function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
-
-  passwordText.value = password;
-
-}
-
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+
+// Write password to the #password input
+function writePassword() {  
+
+//   var password = generatePassword();
+
+//  var passwordText = document.querySelector("#password");
+
+//  passwordText.value = password;
+
+console.log ("in writes password");
+inputCharLength ();
+}
+
+function inputCharLength() {
+  var length = prompt("How many characters would you like your password to contain?");
+  if (length  != null) {
+    document.getElementById("demo").innerHTML =
+    "Hello " + person + "! How are you today?";
+  }
+}
+
+
+// Add event listener to generate button
+
+// generateBtn.addEventListener("click", writePassword);
 
 
 // use prompts to extract input from user 
@@ -26,12 +44,12 @@ generateBtn.addEventListener("click", writePassword);
 // display generated password in alert window or append to page
 
 // make variable of password 8-128 char long
-// make array of lowercase letters
-// make array of uppercase letters
-// make array of numbers
-// make array of special characters
 
+// make array of lowercase letters
 var lowercase = ["q", "w", "e", "r", "t", "y", "u", "i", "o", "p", "a", "s", "d", "f", "g", "h", "j", "k", "l", "z", "x", "c", "v", "b", "n", "m"]
+// make array of uppercase letters
 var uppercase = ["Q", "W", "E", "R", "T", "Y", "U", "I", "O", "P", "A", "S", "D" ,"F" ,"G" ,"H" ,"J", "K", "L", "Z", "X", "C", "V", "B", "N", "M"]
+// make array of numbers
 var nums = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"]
+// make array of special characters
 var special = [" ", "!", "#", "$", "%", "&", "'", "(", ")", "*", "+", ",", "-", ".", "/", ":", ";", "<", "=", ">", "?", "@", "[", "]", "^", "_", "`", "{", "|", "~"]
