77942937c6abf47df995116b6d501d44f5b7777e
John Baxter <juancho420@ymail.com>
2021-03-26 17:40:44 -0700

    some arrays to push

--- script.js
+++ script.js
@@ -30,3 +30,8 @@ generateBtn.addEventListener("click", writePassword);
 // make array of uppercase letters
 // make array of numbers
 // make array of special characters
+
+var lowercase = ["q", "w", "e", "r", "t", "y", "u", "i", "o", "p", "a", "s", "d", "f", "g", "h", "j", "k", "l", "z", "x", "c", "v", "b", "n", "m"]
+var uppercase = ["Q", "W", "E", "R", "T", "Y", "U", "I", "O", "P", "A", "S", "D" ,"F" ,"G" ,"H" ,"J", "K", "L", "Z", "X", "C", "V", "B", "N", "M"]
+var nums = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"]
+var special = [" ", "!", "#", "$", "%", "&", "'", "(", ")", "*", "+", ",", "-", ".", "/", ":", ";", "<", "=", ">", "?", "@", "[", "]", "^", "_", "`", "{", "|", "~"]
